4ec61895d14382e1cdf3417b12b0040d0a5cc087
Kevinwmiguel <kevincursos@outlook.com>
2024-11-23 15:36:55 +0000

    FighT

---
+++ pipex/pipex.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kwillian <kwillian@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/22 17:30:52 by kwillian          #+#    #+#             */
+/*   Updated: 2024/11/22 17:35:26 by kwillian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h> // perror()
+# include <string.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+
+
+
+
+
+
+
+#endif
