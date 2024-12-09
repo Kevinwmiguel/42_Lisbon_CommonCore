867ce88b0aba9a2772268f12a0da0e645ab9c0ea
vboxuser <kwillian@Linux.myguest.virtualbox.org>
2024-12-09 19:26:06 +0000

    upgrade commands

--- pipex/srcs/pipex.c
+++ pipex/srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: kwillian <kwillian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 12:37:03 by kwillian          #+#    #+#             */
-/*   Updated: 2024/11/30 20:12:44 by kwillian         ###   ########.fr       */
+/*   Updated: 2024/12/09 19:12:46 by kwillian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,67 +42,96 @@
 // 				 6         |     end[1]    |  
 // 						   -----------------
 
-check args()
+void	child_one(int fd_read, int fd_write, char *cmd, char **envp)
 {
-	
-}
-
-void	child_one(int fd_write, char *cmd, char **envp)
-{
-	dup2(fd_write, STDOUT_FILENO);
-	close(fd_write);
+    dup2(fd_read, STDIN_FILENO);   // Redireciona stdin para o arquivo de entrada
+    dup2(fd_write, STDOUT_FILENO); // Redireciona stdout para o pipe
+    close(fd_read);
+    close(fd_write);
     char *args[] = {cmd, NULL};
-	char *nova = ft_strjoin("/bin/", cmd);
+    char *nova = ft_strjoin("/bin/", cmd);
     execve(nova, args, envp);
     perror("Execve: ");
     exit(1);
 }
 
-void	child_two(int fd_read, char *cmd, char **envp)
+void	child_two(int fd_read, int fd_write, char *cmd, char **envp)
 {
-	dup2(fd_read, STDIN_FILENO);
-	close(fd_read);
-	char *args[] = {cmd, NULL};
-	char *nova = ft_strjoin("/bin/", cmd);
-	execve(nova, args, envp);
-	perror("Execve: ");
-	exit(1);
+    dup2(fd_read, STDIN_FILENO);  // Redireciona stdin para o pipe
+    dup2(fd_write, STDOUT_FILENO); // Redireciona stdout para o arquivo de saída
+    close(fd_read);
+    close(fd_write);
+    char *args[] = {cmd, NULL};
+    char *nova = ft_strjoin("/bin/", cmd);
+    execve(nova, args, envp);
+    perror("Execve: ");
+    exit(1);
+}
+
+void    checker(int argc, char **envp)
+{
+    if (!envp)
+    {
+        ft_printf("Error\n No Path\n");
+        exit(1);
+    }
+    if (argc != 5)
+    {
+        fprintf(stderr, "Usage: ./pipex infile <cmd1> <cmd2> outfile\n");
+        exit(1);
+    }
 }
 
 int main(int argc, char **argv, char **envp)
 {
 	int end[2];
     pid_t child1, child2;
+    int infile, outfile;
 
-    if (argc != 3)
+    checker(argc, envp);
+    infile = open(argv[1], O_RDONLY);
+    if (infile < 0)
     {
-        fprintf(stderr, "Usage: ./pipex infile <cmd1> <cmd2> outfile\n");
+        perror("Open infile: ");
+        return (1);
+    }
+    outfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (outfile < 0)
+    {
+        perror("Open outfile: ");
+        close(infile);
         return (1);
     }
     if (pipe(end) == -1)
     {
         perror("Pipe: ");
+        close(infile);
+        close(outfile);
         return (1);
     }
+
     child1 = fork();
     if (child1 < 0)
-        return (ft_printf("Fork: "));
+        return (fprintf(stderr, "Fork: "));
     if (child1 == 0)
     {
-        close(end[0]);               // Fecha leitura do pipe
-        child_one(end[1], argv[1], envp); // Executa o primeiro comando
+        close(end[0]); // Fecha leitura do pipe
+        child_one(infile, end[1], argv[2], envp);
     }
+
     child2 = fork();
     if (child2 < 0)
-        return (ft_printf("Fork: "));
+        return (fprintf(stderr, "Fork: "));
     if (child2 == 0)
     {
-        close(end[1]);               // Fecha escrita do pipe
-        child_two(end[0], argv[2], envp); // Executa o segundo comando
+        close(end[1]); // Fecha escrita do pipe
+        child_two(end[0], outfile, argv[3], envp);
     }
-    close(end[0]);                   // Fecha o pipe no pai
+    close(end[0]); // Fecha o pipe no pai
     close(end[1]);
-    waitpid(child1, NULL, 0);        // Espera os filhos terminarem
+    close(infile);
+    close(outfile);
+    waitpid(child1, NULL, 0); // Espera os filhos terminarem
     waitpid(child2, NULL, 0);
     return (0);
 }
--- pipex/srcs/pipex.h
+++ pipex/srcs/pipex.h
@@ -6,7 +6,7 @@
 /*   By: kwillian <kwillian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 12:37:00 by kwillian          #+#    #+#             */
-/*   Updated: 2024/11/30 20:04:58 by kwillian         ###   ########.fr       */
+/*   Updated: 2024/12/09 19:01:21 by kwillian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,5 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
 #endif // DEBUG
